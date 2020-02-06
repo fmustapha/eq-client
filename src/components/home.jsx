b2bed4b3e9bfbac6abf8cbc655f1b88fc3d23956
Faith Mustapha <f81@MacBook-Air.phub.net.cable.rogers.com>
2020-02-06 14:50:17 -0500

    fetch data from the api

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-  state = {  }
+  state = { 
+    error: null,
+    isLoaded: false,
+    stats:[]
+   }
+
+  componentDidMount(){
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "http://localhost:5555/stats/hourly"
+    fetch(proxyurl + url)
+     .then(res => res.json)
+     .then(stats => {
+      //  this.setState({
+      //     stats,
+      //     isLoaded: true
+      //  })
+      console.log(stats, "<--stats")
+     })
+     .catch(error => {
+           this.setState({
+             isLoaded: true,
+             error
+           })
+     });
+  };
+
   render() { 
+    // const { stats } = state 
     return ( <h1>EQ Works</h1> );
   }
 }
