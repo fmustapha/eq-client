541103556f23e3517f7a1c308206e7b8414695e7
Faith Mustapha <f81@MacBook-Air.phub.net.cable.rogers.com>
2020-02-06 11:44:53 -0500

    add navbar and home components

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        EQ Works
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
