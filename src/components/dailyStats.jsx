419507a5ecdc0d301abc7d30727f9436b1fb0f69
Faith Mustapha <f81@MacBook-Air.phub.net.cable.rogers.com>
2020-02-12 12:27:30 -0500

    fix daily stats chart

--- src/components/dailyStats.jsx
+++ src/components/dailyStats.jsx
@@ -21,14 +21,16 @@ const DailyStats = ({ data }) => (
       bottom: 5
     }}
   >
-    <CartesianGrid strokeDasharray="2 2" />
-    <XAxis dataKey="date" />
-    <YAxis />
-    <Tooltip />
-    <Legend />
-    <Bar dataKey="impressions" fill="#8884d8" />
-    <Bar dataKey="clicks" fill="#82ca9d" />
-    <Bar dataKey="revenue" fill="#8664d8" />
+     <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis yAxisId="impressions" orientation="left" stroke="#8884d8" />
+        <YAxis yAxisId="clicks" orientation="right" stroke="#82ca9d" />
+        <YAxis yAxisId="clicks" orientation="revenue" stroke="#c8c6ed" />
+        <Tooltip />
+        <Legend />
+        <Bar yAxisId="impressions" dataKey="impressions" fill="#8884d8" />
+        <Bar yAxisId="clicks" dataKey="clicks" fill="#82ca9d" />
+        <Bar yAxisId="clicks" dataKey="revenue" fill="#c8c6ed" />
   </BarChart>
 );
 
